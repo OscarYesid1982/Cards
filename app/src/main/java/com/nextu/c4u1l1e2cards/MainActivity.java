834eb6fdf85232f7e7354cd803c2d75f727f3ca7
OscarYGilC <oscaryessid2003@gmail.com>
2020-10-21 23:02:25 -0500

    Initial commit

---
+++ app/src/main/java/com/nextu/c4u1l1e2cards/MainActivity.java
@@ -0,0 +1,74 @@
+package com.nextu.c4u1l1e2cards;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/***
+ * Main Activity for the Material Me app, a mock sports news application
+ */
+public class MainActivity extends AppCompatActivity {
+
+    //Member variables
+    private RecyclerView mRecyclerView;
+    private ArrayList<Sport> mSportsData;
+    private SportsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize the RecyclerView
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+
+        //Set the Layout Manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //Initialize the ArrayList that will contain the data
+        mSportsData = new ArrayList<>();
+
+        //Initialize the adapter and set it ot the RecyclerView
+        mAdapter = new SportsAdapter(this, mSportsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        //Get the data
+        initializeData();
+    }
+
+    /**
+     * Method for initializing the sports data from resources.
+     */
+    private void initializeData() {
+        //Get the resources from the XML file
+        String[] sportsList = getResources().getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources().getStringArray(R.array.sports_info);
+        TypedArray sportsImageResources = getResources().obtainTypedArray(R.array.sports_images);
+        //Clear the existing data (to avoid duplication)
+        mSportsData.clear();
+
+
+        //Create the ArrayList of Sports objects with the titles, images
+        // and information about each sport
+        for(int i=0; i<sportsList.length; i++){
+            mSportsData.add(new Sport(sportsList[i], sportsInfo[i],
+                    sportsImageResources.getResourceId(i,0)));
+        }
+
+        //Recycle the typed array
+        sportsImageResources.recycle();
+
+        //Notify the adapter of the change
+        mAdapter.notifyDataSetChanged();
+    }
+
+}
